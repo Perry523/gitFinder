f9c0c53cc379adfacf4c587f4628b9d6342f0726
wendel.renato <wendel.renato@gmail.com>
2020-07-11 22:28:37 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# gitFinder
-A clean and practical github finder
+# gitFinder (git-finder)
+
+A github finder made with quasar
+
+## Install the dependencies
+```bash
+yarn
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+yarn run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.conf.js](https://quasar.dev/quasar-cli/quasar-conf-js).
---
+++ src/pages/Index.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-page>
+  <div class="col-4 d-flex row flex-center content-start">
+    <q-input v-model="userToFind" @keyup.enter="searchUser" class="w-50" type="text" label="Nick do usuário" />
+    <q-btn color="primary" label="Pesquisar"  @click="searchUser" />  
+  </div>
+  <div v-if="userFound" class="d-flex column q-mt-md items-center">
+    <q-avatar size="200px" font-size="52px" color="teal" text-color="white">
+    <img :src="userFound.avatar_url"/>
+    </q-avatar>
+    <p class="q-mt-md text-h4">{{userFound.name ? userFound.name : "Nome Não informado"}}</p>
+    <p >{{userFound.email ? userFound.email : "Email não informado"}}</p>
+    <p>{{userFound.bio ? userFound.bio : 'Bio não informada'}}</p>
+    <p class="text-h6">Repositorios publicos: {{userFound.public_repos}}</p>
+    <div class="row">
+
+    <q-card @click="goToRepository(i)" v-for="(repository,i) in repositories" :key="i" class="q-ma-sm col-auto my-card">
+      <div class="q-pa-sm d-flex column">
+        <a class="text-h6">{{repository.name}}</a>
+        <div class="row justify-center " style="height:15px">
+          <q-icon class="q-mt-xs text-orange" name="stars" />
+        <p class="text-h8">{{repository.stargazers_count}}</p>
+          <q-icon class="q-mt-xs q-ml-auto" name="remove_red_eye" />
+        <p class="text-h8 q-mr-auto">{{repository.watchers_count}}</p>
+          <q-icon class="q-mt-xs text-red" name="account_tree" />
+        <p class="text-h8">{{repository.forks}}</p>
+        </div>
+      </div>
+    </q-card>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      userToFind: null,
+      repositories: null,
+      userFound: null,
+    }
+  },
+  methods: {
+    goToRepository(i){
+      window.location = this.repositories[i].html_url
+    },
+    async searchUser(){
+      this.userFound = await this.$http(`/${this.userToFind}`).then(response => response.data)
+      this.repositories = await this.$http(this.userFound.repos_url).then(response => response.data)
+    }
+  },
+}
+</script>
+<style scoped>
+.w-50{
+  width: 50%;
+  
+}
+.my-card{
+  cursor: pointer;
+}
+</style>
